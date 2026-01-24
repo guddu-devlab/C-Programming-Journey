f64c3c000b6a1efc583629b7ca765b15d39519d1
Guddu <gudduwin62@gmail.com>
2026-01-24 22:48:49 +0530

    Lesson 03: Conditional execution using if-else

---
+++ basics/lesson-03-if-else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int number = 7;  
+    
+    if (number > 5) {
+        // This block runs if the condition is true
+        printf("Number is greater than 5\n");
+    } else {
+        // This block runs if the condition is false
+        printf("Number is 5 or less\n");
+    }
+
+    return 0;  
+}
